9eca87e06ff9bc644e91cc489646ba1d31a855dd
jibon-roy <mailme.jibon@gmail.com>
2024-07-01 00:12:18 +0600

    arrowplane valocity controling

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 const canvas = document.getElementById("canvas");
+const plane = document.getElementById("plane");
 
-const ctx = canvas.getContext("2d");
+plane.style.position = "absolute";
+plane.style.left = "100px";
+plane.style.top = "100px";
+
+const canvasHeight = canvas.offsetHeight;
+
+let timer; // current timeout id to clear
+function come() {
+  /* do something */
+}
+let time; // dynamic interval
+
+(function repeat() {
+  come();
+  timer = setTimeout(repeat, time / 100);
+
+  if (timer <= canvasHeight - 270) plane.style.top = 200 + timer + "px";
+})();
